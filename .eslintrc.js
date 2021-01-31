fd5ca9b094d8e6ba3baeebdafcb9994826846b0b
Polle <chip.mobil@gmail.com>
2021-01-31 13:29:53 +0100

    eslint working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
     root: true,
     env: {
-      node: true,
+        node: true,
     },
     parser: '@typescript-eslint/parser',
     parserOptions: {
-      ecmaVersion: 6,
-      sourceType: "module",
+        ecmaVersion: 6,
+        sourceType: 'module',
     },
-    plugins: [
-        '@typescript-eslint',
-      ],
-      extends: ['eslint:recommended',
-      'plugin:@typescript-eslint/recommended',],
-    rules: {
-      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    },
-  };
-  
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-setInterval(()=>{
-console.log("hiho")
-},1000)
+setInterval(() => {
+    console.log('hiho');
+}, 1000);
