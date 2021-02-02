5a7345c9fb9508d94722ba6ce5191f99c2fe6b1f
Polle <chip.mobil@gmail.com>
2021-02-02 12:46:56 +0100

    refactor connect and use server

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,42 +1,37 @@
-import { TeamSpeak, TeamSpeakServer, TeamSpeakChannel, TeamSpeakServerGroup } from 'ts3-nodejs-library';
+import { TeamSpeak, TeamSpeakChannel, TeamSpeakServerGroup } from 'ts3-nodejs-library';
 import { ClientConnect, ClientDisconnect, ClientMoved } from 'ts3-nodejs-library/lib/types/Events';
 import { TSExitCode } from '../enums';
 
 export class Bot {
-    private _teamSpeakHandle: TeamSpeak;
+    private _teamSpeak: TeamSpeak;
     public constructor(teamSpeakHandle: TeamSpeak) {
-        this._teamSpeakHandle = teamSpeakHandle;
-        this._teamSpeakHandle.on('ready', this.init.bind(this));
+        this._teamSpeak = teamSpeakHandle;
+        this._teamSpeak.on('ready', this.init.bind(this));
     }
     private async init() {
-        const teamSpeakServerHandle = (await this._teamSpeakHandle.serverList()).find(
-            (item: TeamSpeakServer) => item.port === this._teamSpeakHandle.config.serverport,
-        );
-        if (!teamSpeakServerHandle) return process.exit(TSExitCode.VirtualServerNotFound);
-        await teamSpeakServerHandle.use(process.env.TS_NICKNAME || 'serveradmin');
-        await this._teamSpeakHandle.useBySid(teamSpeakServerHandle.id);
-        return;
+        if (!this._teamSpeak.config.serverport) return;
+        await this._teamSpeak.useByPort(this._teamSpeak.config.serverport, process.env.TS_NICKNAME || 'serveradmin');
     }
     public async getDefaultChannel(): Promise<TeamSpeakChannel> {
-        const defaultChannel = (await this._teamSpeakHandle.channelList()).find((item) => item.flagDefault === true);
+        const defaultChannel = (await this._teamSpeak.channelList()).find((item) => item.flagDefault === true);
         if (!defaultChannel) return process.exit(TSExitCode.ChannelNotFound);
         return defaultChannel;
     }
     public async getGroupByName(name: string): Promise<TeamSpeakServerGroup> {
-        const group = await this._teamSpeakHandle.getServerGroupByName(name);
+        const group = await this._teamSpeak.getServerGroupByName(name);
         if (!group) return process.exit(TSExitCode.GroupNotFound);
         return group;
     }
 
     public onClientConnect(func: (event: ClientConnect) => void): void {
-        this._teamSpeakHandle.on('clientconnect', func);
+        this._teamSpeak.on('clientconnect', func);
     }
 
     public onClientDisconnect(func: (event: ClientDisconnect) => void): void {
-        this._teamSpeakHandle.on('clientdisconnect', func);
+        this._teamSpeak.on('clientdisconnect', func);
     }
 
     public onClientMoved(func: (event: ClientMoved) => void): void {
-        this._teamSpeakHandle.on('clientmoved', func);
+        this._teamSpeak.on('clientmoved', func);
     }
 }
