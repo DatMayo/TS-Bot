48f24701a69793fc601851a9a6f093169461f8c4
Mario Franze <mario.franze@gmail.com>
2021-02-05 09:26:37 +0100

    Added method comments

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -20,19 +20,30 @@ export class Bot {
             this._teamSpeakHandle.config.nickname,
         );
     }
-
+    /**
+     * Returns a handle to the default TS Channel
+     * @returns Promise<TeamSpeakChannel> Handle of the default channel
+     */
     public async getDefaultChannel(): Promise<TeamSpeakChannel> {
         const defaultChannel = (await this._teamSpeakHandle.channelList()).find((item) => item.flagDefault === true);
         if (!defaultChannel) return process.exit(TSExitCode.ChannelNotFound);
         return defaultChannel;
     }
-
+    /**
+     * Gets a channel for the given channelname
+     * @param  {string} name Name to search for
+     * @returns Promise<TeamSpeakChannel> Handle of the searched channel
+     */
     public async getChannelByName(name: string): Promise<TeamSpeakChannel> {
         const channel = await this._teamSpeakHandle.getChannelByName(name);
         if (!channel) return process.exit(TSExitCode.ChannelNotFound);
         return channel;
     }
-
+    /**
+     * Gets a channel group by the given name
+     * @param  {string} name Name to search for
+     * @returns Promise<TeamSpeakServerGroup> Handle to the server group
+     */
     public async getGroupByName(name: string): Promise<TeamSpeakServerGroup> {
         const group = await this._teamSpeakHandle.getServerGroupByName(name);
         if (!group) return process.exit(TSExitCode.GroupNotFound);
